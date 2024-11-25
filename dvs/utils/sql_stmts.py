68fdde2b30e8d23299018d80c82da60b9daf1845
Allen Chou <f1470891079@gmail.com>
2024-11-25 18:49:40 +0800

    Add SQL display utilities and enhance SQL statement handling
    - Introduced `display.py` with functions for formatted output of SQL queries and parameters.
    - Added `openapi.py` to generate SQL CREATE TABLE statements, supporting JSON types and various SQL data types.
    - Updated `sql_stmts.py` to use `Final` for SQL statement constants, improving code clarity and ensuring immutability.

--- dvs/utils/sql_stmts.py
+++ dvs/utils/sql_stmts.py
@@ -1,7 +1,8 @@
 from textwrap import dedent
+from typing import Final, Text
 
 # SQL statement for vector search
-SQL_STMT_VSS = dedent(
+SQL_STMT_VSS: Final[Text] = dedent(
     """
     WITH vector_search AS (
         SELECT {column_names_expr}, array_cosine_similarity(embedding, ?::FLOAT[{embedding_dimensions}]) AS relevance_score
@@ -15,3 +16,35 @@ SQL_STMT_VSS = dedent(
     ORDER BY p.relevance_score DESC
     """  # noqa: E501
 ).strip()
+
+# SQL Show tables
+SQL_STMT_SHOW_TABLES: Final[Text] = "SHOW TABLES"
+
+# SQL Drop table
+SQL_STMT_DROP_TABLE: Final[Text] = "DROP TABLE IF EXISTS {{ table_name }} CASCADE"
+
+# SQL Remove outdated points
+SQL_STMT_REMOVE_OUTDATED_POINTS: Final[Text] = dedent(
+    """
+    DELETE FROM {{ table_name }} WHERE document_id = ? AND content_md5 != ?
+    """
+).strip()
+
+# SQL Get by doc ids
+SQL_STMT_GET_BY_DOC_IDS: Final[Text] = dedent(
+    """
+    SELECT {{ columns_expr }} FROM {{ table_name }} WHERE document_id IN ( {{ placeholders }} )
+    """  # noqa: E501
+).strip()
+
+# SQL Create index
+SQL_STMT_CREATE_INDEX: Final[Text] = (
+    "CREATE INDEX idx_{table_name}_{column_name} ON {table_name} ({column_name});"
+)
+
+# SQL Create embedding index
+SQL_STMT_CREATE_EMBEDDING_INDEX: Final[Text] = (
+    "CREATE INDEX idx_{table_name}_{column_name} ON {table_name} "
+    + "USING HNSW({column_name}) "
+    + "WITH (metric = '{metric}');"  # You can choose 'l2sq' or 'ip' instead of 'cosine' if needed  # noqa: E501
+)
