ad7e8bc076098887a62eee8447b4f1870531b268
Allen Chou <f1470891079@gmail.com>
2024-11-26 22:55:00 +0800

    Enhance Point class with embedding functionality and add chunk utility
    - Introduce `set_embeddings_from_contents` method to the Point class
      for setting embeddings based on contents using OpenAI's API.
    - Implement batching for embedding requests with optional debug output.
    - Refactor imports for clarity and consistency.
    - Add new utility module `dvs/utils/chunk.py` with a `chunks` function
      to facilitate processing of large datasets in manageable sizes.

--- dvs/types/point.py
+++ dvs/types/point.py
@@ -1,9 +1,17 @@
-from typing import Any, ClassVar, Dict, List, Optional, Text
+from typing import TYPE_CHECKING, Any, ClassVar, Dict, List, Optional, Sequence, Text
 
 from pydantic import BaseModel, Field
+from tqdm import tqdm
 
-from dvs.utils.ids import get_id
+import dvs.utils.chunk
+import dvs.utils.ids
+from dvs.config import settings
 from dvs.utils.qs import PointQuerySet, PointQuerySetDescriptor
+
+if TYPE_CHECKING:
+    from openai import OpenAI
+
+    from dvs.types.document import Document
 
 
 class Point(BaseModel):
@@ -25,7 +33,7 @@ class Point(BaseModel):
     """  # noqa: E501
 
     point_id: Text = Field(
-        default_factory=lambda: get_id("pt"),
+        default_factory=lambda: dvs.utils.ids.get_id("pt"),
         description="Unique identifier for the point in the vector space.",
     )
     document_id: Text = Field(
@@ -51,6 +59,55 @@ class Point(BaseModel):
     @classmethod
     def query_set(cls) -> "PointQuerySet":
         return PointQuerySet(cls)
+
+    @classmethod
+    def set_embeddings_from_contents(
+        cls,
+        points: Sequence["Point"],
+        contents: Sequence[Text] | Sequence["Document"],
+        *,
+        openai_client: "OpenAI",
+        batch_size: int = 500,
+        debug: bool = False,
+    ) -> Sequence["Point"]:
+        if len(points) != len(contents):
+            raise ValueError("Points and contents must be the same length")
+
+        _iter_chunks = zip(
+            dvs.utils.chunk.chunks(points, batch_size),
+            dvs.utils.chunk.chunks(contents, batch_size),
+        )
+        if debug is True:
+            _iter_chunks = tqdm(
+                _iter_chunks,
+                desc="Creating embeddings",
+                total=len(points) // batch_size + 1,
+                leave=False,
+            )
+
+        # Create embeddings
+        for batched_points, batched_contents in _iter_chunks:
+            _contents: List[Text] = [
+                (
+                    content.strip()
+                    if isinstance(content, Text)
+                    else content.content.strip()
+                )
+                for content in batched_contents
+            ]
+            emb_res = openai_client.embeddings.create(
+                input=_contents,
+                model=settings.EMBEDDING_MODEL,
+                dimensions=settings.EMBEDDING_DIMENSIONS,
+            )
+            if len(emb_res.data) != len(batched_points):
+                raise ValueError(
+                    "Embedding response and points must be the same length"
+                )
+            for point, embedding in zip(batched_points, emb_res.data):
+                point.embedding = embedding.embedding
+
+        return points
 
     @property
     def is_embedded(self) -> bool:
