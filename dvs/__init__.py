f21e3cacad5dc45d4a35020a0478644ca81e8056
AllenChou <f1470891079@gmail.com>
2024-12-04 13:57:24 +0800

    Enhance DVS class with document management and caching
    - Add new imports for Settings, Document, and Point to the public API.
    - Update __all__ to include new components for module accessibility.
    - Modify DVS constructor to accept openai_client and cache parameters.
    - Introduce add and remove methods for managing documents and points.
    - Integrate diskcache for improved performance and OpenAI for embeddings.

--- dvs/__init__.py
+++ dvs/__init__.py
@@ -1,6 +1,15 @@
 from ._dvs import DVS
+from .config import Settings, settings
+from .types.document import Document
+from .types.point import Point
 from .version import VERSION
 
 __version__ = VERSION
 
-__all__ = ["DVS"]
+__all__ = [
+    "DVS",
+    "Document",
+    "Point",
+    "Settings",
+    "settings",
+]
--- dvs/_dvs.py
+++ dvs/_dvs.py
@@ -1,11 +1,15 @@
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional, Text, Tuple, Union
 
+import diskcache
 import duckdb
+from openai import OpenAI
 
+import dvs.utils.vss as VSS
 from dvs.config import settings
 from dvs.types.document import Document
 from dvs.types.point import Point
+from dvs.types.search_request import SearchRequest
 
 
 class DVS:
@@ -15,10 +19,16 @@ class DVS:
         *,
         touch: bool = True,
         raise_if_exists: bool = False,
-        debug: bool = False
+        debug: bool = False,
+        openai_client: Optional["OpenAI"] = None,
+        cache: Optional["diskcache.Cache"] = None,
     ):
         self._db_path = duckdb_path or Path(settings.DUCKDB_PATH)
         self.debug = debug
+        self.openai_client = openai_client or OpenAI(api_key=settings.OPENAI_API_KEY)
+        self.cache = cache or diskcache.Cache(
+            settings.CACHE_PATH, size_limit=settings.CACHE_SIZE_LIMIT
+        )
 
         if touch:
             self.touch(raise_if_exists=raise_if_exists, debug=debug)
@@ -31,10 +41,84 @@ class DVS:
     def conn(self) -> duckdb.DuckDBPyConnection:
         return duckdb.connect(self._db_path)  # Always open a new duckdb connection
 
-    def touch(self, *, raise_if_exists: bool = False, debug: bool = False):
+    def touch(self, *, raise_if_exists: bool = False, debug: Optional[bool] = None):
+        """"""
+
+        debug = self.debug if debug is None else debug
+
         Document.objects.touch(
             conn=self.conn, raise_if_exists=raise_if_exists, debug=debug
         )
         Point.objects.touch(
             conn=self.conn, raise_if_exists=raise_if_exists, debug=debug
         )
+
+    def add(
+        self, documents: List[Document], *, debug: Optional[bool] = None
+    ) -> List[Tuple[Document, List[Point]]]:
+        """"""
+
+        debug = self.debug if debug is None else debug
+        output: List[Tuple[Document, List[Point]]] = []
+
+        # Create documents and points
+        docs = Document.objects.bulk_create(documents, conn=self.conn, debug=debug)
+        for doc in docs:
+            points: List[Point] = doc.to_points()
+            output.append((doc, points))
+
+        # Create embeddings
+        all_points = [pt for _, pts in output for pt in pts]
+        all_points = Point.set_embeddings_from_contents(
+            all_points,
+            docs,
+            openai_client=self.openai_client,
+            cache=self.cache,
+            debug=debug,
+        )
+
+        # Bulk create documents and points
+        docs = Document.objects.bulk_create(docs, conn=self.conn, debug=debug)
+        all_points = Point.objects.bulk_create(all_points, conn=self.conn, debug=debug)
+
+        return output
+
+    def remove(self, doc_ids: Union[Text, List[Text]], *, debug: Optional[bool] = None):
+        """"""
+
+        pass
+
+    async def search(
+        self,
+        query: Text,
+        top_k: int = 3,
+        *,
+        with_embedding: bool = False,
+        debug: Optional[bool] = None,
+    ) -> List[Tuple["Point", Optional["Document"], float]]:
+        """"""
+
+        query = query.strip()
+        if not query:
+            raise ValueError("Query cannot be empty")
+
+        # Validate search request
+        search_req = SearchRequest.model_validate(
+            {"query": query, "top_k": top_k, "with_embedding": with_embedding}
+        )
+        vectors = await SearchRequest.to_vectors(
+            [search_req],
+            cache=self.cache,
+            openai_client=self.openai_client,
+        )
+        vector = vectors[0]
+
+        # Perform vector search
+        results = await VSS.vector_search(
+            vector=vector,
+            top_k=search_req.top_k,
+            conn=self.conn,
+            with_embedding=search_req.with_embedding,
+        )
+
+        return results
