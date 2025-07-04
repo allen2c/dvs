19a6ed33bfc86d38be29f5e6085e10cfb820cc78
Allen Chou <f1470891079@gmail.com>
2025-07-04 19:02:43 +0800

    refactor(point): ♻️ Modernize embedding generation
    Revamp the 'set_embeddings_from_contents' method in the Point class to leverage the new 'openai_embeddings_model' library.
    This change streamlines the embedding creation process by centralizing model interactions and removes direct dependencies on OpenAI client, tqdm, and diskcache within this method, promoting a cleaner and more modular design.

--- dvs/types/point.py
+++ dvs/types/point.py
@@ -1,32 +1,21 @@
-from typing import (
-    TYPE_CHECKING,
-    Any,
-    ClassVar,
-    Dict,
-    List,
-    Optional,
-    Sequence,
-    Set,
-    Text,
-)
+import logging
+import typing
 
-from pydantic import BaseModel, Field
-from tqdm import tqdm
+import openai_embeddings_model as oai_emb_model
+import pydantic
 
 import dvs.utils.cache
 import dvs.utils.chunk
 import dvs.utils.ids
 import dvs.utils.qs
-from dvs.config import console, settings
 
-if TYPE_CHECKING:
-    import diskcache
-    from openai import OpenAI
-
+if typing.TYPE_CHECKING:
     from dvs.types.document import Document
 
+logger = logging.getLogger(__name__)
 
-class Point(BaseModel):
+
+class Point(pydantic.BaseModel):
     """
     Represents a point in the vector space, associated with a document.
 
@@ -44,127 +33,63 @@ class Point(BaseModel):
     the embedding that is used for comparison with query vectors.
     """  # noqa: E501
 
-    point_id: Text = Field(
+    model_config = pydantic.ConfigDict(validate_assignment=True)
+
+    # Fields
+    point_id: typing.Text = pydantic.Field(
         default_factory=lambda: dvs.utils.ids.get_id("pt"),
         description="Unique identifier for the point in the vector space.",
     )
-    document_id: Text = Field(
+    document_id: typing.Text = pydantic.Field(
         ...,
         description="Identifier of the associated document.",
     )
-    content_md5: Text = Field(
+    content_md5: typing.Text = pydantic.Field(
         ...,
         description="MD5 hash of the content for quick comparison and integrity checks.",  # noqa: E501
     )
-    embedding: List[float] = Field(
+    embedding: typing.List[float] | typing.Sequence[typing.Text] = pydantic.Field(
         default_factory=list,
-        max_length=settings.EMBEDDING_DIMENSIONS,
-        description="Vector embedding representation of the point.",
+        description=(
+            "Vector embedding representation of the point. "
+            + "Python list of floats (float64) or "
+            + "base64 string of numpy array in float32."
+        ),
     )
-    metadata: Dict[Text, Any] = Field(
+
+    metadata: typing.Dict[typing.Text, typing.Any] = pydantic.Field(
         default_factory=dict,
         description="Additional metadata associated with the point.",
     )
 
-    # Class variables
-    objects: ClassVar[dvs.utils.qs.PointQuerySetDescriptor] = (
-        dvs.utils.qs.PointQuerySetDescriptor()
-    )
-
-    @classmethod
-    def query_set(cls) -> dvs.utils.qs.PointQuerySet:
-        return dvs.utils.qs.PointQuerySet(cls)
-
     @classmethod
     def set_embeddings_from_contents(
         cls,
-        points: Sequence["Point"],
-        contents: Sequence[Text] | Sequence["Document"],
+        points: typing.Sequence["Point"],
+        contents: typing.Sequence[typing.Text] | typing.Sequence["Document"],
         *,
-        openai_client: "OpenAI" = settings.openai_client,
-        batch_size: int = 500,
-        cache: Optional["diskcache.Cache"] = settings.cache,
-        debug: bool = False,
-    ) -> List["Point"]:
+        model: oai_emb_model.OpenAIEmbeddingsModel,
+        model_settings: oai_emb_model.ModelSettings,
+    ) -> typing.List["Point"]:
+        """
+        Set the embeddings for the points from the contents.
+        """
         if len(points) != len(contents):
             raise ValueError("Points and contents must be the same length")
 
-        _iter_chunks = zip(
-            dvs.utils.chunk.chunks(points, batch_size),
-            dvs.utils.chunk.chunks(contents, batch_size),
+        output: typing.List["Point"] = []
+
+        _contents: typing.List[typing.Text] = [
+            c if isinstance(c, str) else c.content for c in contents
+        ]
+        _embeddings_resp = model.get_embeddings(
+            input=_contents, model_settings=model_settings
         )
-        if debug is True:
-            _iter_chunks = tqdm(
-                _iter_chunks,
-                desc="Creating embeddings",
-                total=len(points) // batch_size + 1,
-                leave=False,
-            )
+        for point, embedding in zip(points, _embeddings_resp.to_python()):
+            point.embedding = embedding
+            output.append(point)
 
-        # Create embeddings
-        cache_hits_count = 0
-        for batched_points, batched_contents in _iter_chunks:
-            # Ensure contents are clean strings
-            _contents: List[Text] = [
-                (
-                    content.strip()
-                    if isinstance(content, Text)
-                    else content.content.strip()
-                )
-                for content in batched_contents
-            ]
-
-            # Check cache
-            _cache_hit: Set[int] = set()
-            if cache is not None:
-                for _idx, (_point, _content) in enumerate(
-                    zip(batched_points, _contents)
-                ):
-                    _cached_embedding = cache.get(
-                        dvs.utils.cache.get_embedding_cache_key(_content)
-                    )
-                    if _cached_embedding is not None:
-                        _point.embedding = _cached_embedding  # type: ignore
-                        _cache_hit.add(_idx)
-            cache_hits_count += len(_cache_hit)
-
-            # Create embeddings
-            _contents_to_embed = [
-                c for idx, c in enumerate(_contents) if idx not in _cache_hit
-            ]
-            if len(_contents_to_embed) > 0:
-                emb_res = openai_client.embeddings.create(
-                    input=_contents_to_embed,
-                    model=settings.EMBEDDING_MODEL,
-                    dimensions=settings.EMBEDDING_DIMENSIONS,
-                )
-                for point, content, embedding in zip(
-                    [
-                        pt
-                        for idx, pt in enumerate(batched_points)
-                        if idx not in _cache_hit
-                    ],
-                    _contents_to_embed,
-                    emb_res.data,
-                ):
-                    point.embedding = embedding.embedding
-                    if cache is not None:
-                        cache.set(
-                            dvs.utils.cache.get_embedding_cache_key(content),
-                            embedding.embedding,
-                        )
-
-        output = list(points)
-        if any(
-            pt.is_embedded is False
-            or len(pt.embedding) != settings.EMBEDDING_DIMENSIONS
-            for pt in output
-        ):
-            raise ValueError("Not all points were embedded, it is programmer error")
-        if debug is True:
-            console.print(f"Created {len(output)} embeddings")
-            if cache is not None:
-                console.print(f"Cache hits: {cache_hits_count} / {len(output)}")
+        logger.debug(f"Created {len(output)} embeddings")
         return output
 
     @property
